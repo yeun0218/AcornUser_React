7f244620170a337b08aadb58717e731559d41cfb
yye0218 <dpdmsdl52@naver.com>
2025-01-02 12:24:23 +0900

    20250102 로그인 리액트 & 라우터 수정

--- src/components/member/login/NaverLogin.jsx
+++ src/components/member/login/NaverLogin.jsx
@@ -16,7 +16,7 @@ const NaverLogin = (props) => {
   const btnNaver = async () => {
     const naverLogin = await new naver.LoginWithNaverId({
       clientId: "RHVEy9MDa5Qd9ly1kZmA",
-      callbackUrl: "http://localhost:3002/naverlogin",
+      callbackUrl: "http://localhost:3000/naverlogin",
       isPopup: false, // popup 형식으로 띄울것인지 설정
       loginButton: { color: "green", type: 3, height: "45" }, //버튼의 스타일, 타입, 크기를 지정
     });
