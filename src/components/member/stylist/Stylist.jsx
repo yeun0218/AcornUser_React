1cf7e9d2fe2120a103e897f33bb2d1f3a885523c
yulzzangzzang <106054124+heedahee@users.noreply.github.com>
2025-01-02 17:41:44 +0900

    20250102-디자이너소개 프론트 개발

--- src/components/member/stylist/Stylist.jsx
+++ src/components/member/stylist/Stylist.jsx
@@ -1,17 +1,13 @@
 import React from "react";
-import BrandStory from "../aboutshop/BrandStory";
-import Business from "../aboutshop/Business";
-import Story from "../aboutshop/Story";
 import ContactUs from "../aboutshop/ContactUs";
 import Header from "../../common/Header.jsx";
+import DesignerTab from "./DesignerTab";
 
 const About = () => {
     return (
         <div>
             <Header />
-            <BrandStory />
-            <Business />
-            <Story />
+            <DesignerTab />
             <ContactUs />
         </div>
     );
